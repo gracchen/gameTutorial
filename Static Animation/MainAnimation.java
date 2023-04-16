dde6a684fb971b4ba831e19e2a5c87e48ae25010
gracchen <gracetheartist@g.ucla.edu>
2023-04-16 00:07:36 -0700

    auto-select compatible resolution

--- Static Animation/MainAnimation.java
+++ Static Animation/MainAnimation.java
@@ -2,10 +2,11 @@ package FullScreen;
 
 import java.awt.DisplayMode;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MainAnimation {
@@ -13,47 +14,117 @@ public class MainAnimation {
 	private ArrayList<Image> frames;
 	private Image bg; //background
 	private Animation a;
-	
+
 	public static void main(String[] args) {
-		DisplayMode dm = new DisplayMode(800,600,16, DisplayMode.REFRESH_RATE_UNKNOWN);
 		MainAnimation b = new MainAnimation();
-		
-		b.run(dm);   
+		b.run();
 	}
-	
+
+	private static final DisplayMode modes[] = {
+			new DisplayMode(1920,1080,32,0),
+			new DisplayMode(1920,1080,24,0),
+			new DisplayMode(1920,1080,16,0),
+
+			new DisplayMode(1680,1050,32,0),
+			new DisplayMode(1680,1050,24,0),
+			new DisplayMode(1680,1050,16,0),
+
+			new DisplayMode(1600,900,32,0),
+			new DisplayMode(1600,900,24,0),
+			new DisplayMode(1600,900,16,0),
+
+			new DisplayMode(1440,900,32,0),
+			new DisplayMode(1440,900,24,0),
+			new DisplayMode(1440,900,16,0),
+
+			new DisplayMode(1400,1050,32,0),
+			new DisplayMode(1400,1050,24,0),
+			new DisplayMode(1400,1050,16,0),
+
+			new DisplayMode(1366,768,32,0),
+			new DisplayMode(1366,768,24,0),
+			new DisplayMode(1366,768,16,0),
+
+			new DisplayMode(1360,768,32,0),
+			new DisplayMode(1360,768,24,0),
+			new DisplayMode(1360,768,16,0),
+
+			new DisplayMode(1280,1024,32,0),
+			new DisplayMode(1280,1024,24,0),
+			new DisplayMode(1280,1024,16,0),
+
+			new DisplayMode(1280,960,32,0),
+			new DisplayMode(1280,960,24,0),
+			new DisplayMode(1280,960,16,0),
+
+			new DisplayMode(1280,800,32,0),
+			new DisplayMode(1280,800,24,0),
+			new DisplayMode(1280,800,16,0),
+
+			new DisplayMode(1280,768,32,0),
+			new DisplayMode(1280,768,24,0),
+			new DisplayMode(1280,768,16,0),
+
+			new DisplayMode(1280,720,32,0),
+			new DisplayMode(1280,720,24,0),
+			new DisplayMode(1280,720,16,0),
+
+			new DisplayMode(1152,864,32,0),
+			new DisplayMode(1152,864,24,0),
+			new DisplayMode(1152,864,16,0),
+
+			new DisplayMode(1024,768,32,0),
+			new DisplayMode(1024,768,24,0),
+			new DisplayMode(1024,768,16,0),
+
+			new DisplayMode(800,600,32,0),
+			new DisplayMode(800,600,24,0),
+			new DisplayMode(800,600,16,0),
+
+			new DisplayMode(640,480,32,0),
+			new DisplayMode(640,480,24,0),
+			new DisplayMode(640,480,16,0),
+	};
+
 	public void loadPics() {
-		/* for some reason loading from resource wayyyyyy slower than abs path:
+		//for some reason loading from resource wayyyyyy slower than abs path:
 		a = new Animation(5000, this);
-		bg = new ImageIcon("C:\\Users\\thankYouGod\\Pictures\\java\\" + "back.png").getImage();	
+		a = new Animation(5000,this);
+		bg = new ImageIcon("C:\\Users\\thankYouGod\\Pictures\\java\\" + "back.png").getImage();
 		for (int i = 1; i <= 3; i++) {
 			frames.add(new ImageIcon("C:\\Users\\thankYouGod\\Pictures\\java\\" + "face" + i + ".png").getImage());
 			a.addScene( 601);
-		}*/
-		
-		a = new Animation(6000, this);
-		bg = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/back.png"));
-		
-		for (int i = 1; i <= 3; i++) {
-			frames.add(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/face" + i + ".png")));
-			a.addScene(601);
 		}
+
+		/*a = new Animation(6000, this);
+               bg = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/back.png"));
+
+               for (int i = 1; i <= 3; i++) {
+                       frames.add(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/resources/face" + i + ".png")));
+                        a.addScene(601);
+                }
+		 */
 	}
-	
-	public void run(DisplayMode dm) {
+
+	public void run() {
 		frames = new ArrayList<Image>();
 		screen = new Screen();
 		try {
-			screen.setFullScreen(dm, new JFrame());
-			loadPics();
-			a.start();
+			DisplayMode dm = screen.findFirstCompatibleMode(modes); //finds perfect displaymode
+			System.out.println("Perfect Displaymode: " + dm.getWidth() + "x" + dm.getHeight() + " @ " + dm.getBitDepth());
+			screen.setFullScreen(dm); //sets that displaymode to fullscreen
+			loadPics(); //sets up animation frames
+			a.start(); //starts animation
 		} finally {
 			screen.restoreScreen();
 		}
 	}
-	
+
 	public void draw(int framesIndex) {
-		Graphics g = screen.getFullScreenWindow().getGraphics();
+		Graphics2D g = screen.getGraphics();
 		g.drawImage(bg, 0, 0, null); //MUST be first, otherwise overlaps frame
 		g.drawImage(frames.get(framesIndex), 0, 0, null);
+		g.dispose(); 
+		screen.update(); 
 	}
 }
--- Static Animation/Screen.java
+++ Static Animation/Screen.java
@@ -1,32 +1,87 @@
 package FullScreen;
 
 import java.awt.DisplayMode;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Window;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
 public class Screen {
 	private GraphicsDevice vc; //card
-	
+
+	//let vc access monitor
 	public Screen() {
 		//need environment of vc (like struct of state)
 		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		vc = env.getDefaultScreenDevice();
-		
 	}
-	public void setFullScreen(DisplayMode dm, JFrame window) {
+	
+	public void setFullScreen(DisplayMode dm) {
+		JFrame f = new JFrame();
+		f.setUndecorated(true);
+		f.setIgnoreRepaint(true);
+		f.setResizable(false);
+		vc.setFullScreenWindow(f);
 		
-		window.setUndecorated(true); //purely one screen, no titlebar etc
-		window.setResizable(false);
-		vc.setFullScreenWindow(window); //wrapper for this function that does bulk of work
-		
-		//resolution, bit depth, bit rate
-		if (dm != null && vc.isDisplayChangeSupported()) { //if any variables + allowed to change display
+		if(dm != null && vc.isDisplayChangeSupported()) {
 			try {
 				vc.setDisplayMode(dm);
-			} catch (Exception e) {}
+			} catch (Exception e) {};
+		}
+		f.createBufferStrategy(2); //two drawings, flip between two
+	}
+
+	public DisplayMode[] getCompatibleDisplayModes() { 
+		return vc.getDisplayModes();
+	}
+
+	public DisplayMode findFirstCompatibleMode(DisplayMode modes[]) {
+		DisplayMode goodModes[] = vc.getDisplayModes();
+		for (int i = 0; i < modes.length; i++) {
+			for (int j = 0; j < goodModes.length; j++) {
+				if (displayModesMatch(modes[i], goodModes[j])) {
+					return modes[i]; //compatible with both program and graphics card
+				}
+			}
+		}
+		return null;
+	}
+	
+	public DisplayMode getCurrentDisplayMode() {
+		return vc.getDisplayMode();
+	}
+	
+	public boolean displayModesMatch(DisplayMode m1, DisplayMode m2) {
+		if(m1.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI && m2.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI && m1.getBitDepth() != m2.getBitDepth()) 
+			return false;
+		if(m1.getHeight() != m2.getHeight() || m1.getWidth() != m2.getWidth()) 
+			return false;
+		if(m1.getRefreshRate() != DisplayMode.REFRESH_RATE_UNKNOWN && m2.getRefreshRate()!= DisplayMode.REFRESH_RATE_UNKNOWN && m2.getRefreshRate() != m1.getRefreshRate()) 
+			return false;
+		return true;
+	}
+	
+	public Graphics2D getGraphics() {
+		Window w = vc.getFullScreenWindow(); //storse everything in f into this
+		if (w != null) {
+			BufferStrategy s = w.getBufferStrategy();
+			return (Graphics2D)s.getDrawGraphics();
+		}
+		return null;
+	}
+	
+	public void update() {
+		Window w = vc.getFullScreenWindow();
+		if(w != null) {
+			BufferStrategy s = w.getBufferStrategy();
+			if (!s.contentsLost()) { //if not lost, have stuff to look at
+				s.show();
+			}
 		}
 	}
 	
@@ -34,11 +89,30 @@ public class Screen {
 		return vc.getFullScreenWindow();
 	}
 	
+	public int getWidth() {
+		Window w = vc.getFullScreenWindow();
+		if (w != null) return w.getWidth();
+		return 0;
+	}
+	
+	public int getHeight() {
+		Window w = vc.getFullScreenWindow();
+		if (w != null) return w.getHeight();
+		return 0;
+	}
+	
 	public void restoreScreen() {
 		Window w = vc.getFullScreenWindow();
-		if (w != null) {
-			w.dispose(); //freeing up resources when closing window
+		if (w!= null) w.dispose();
+		vc.setFullScreenWindow(null);
+	}
+	
+	public BufferedImage createCompatibleImage(int w, int h , int t) { //width, height, transparency
+		Window win = vc.getFullScreenWindow();
+		if (win != null) {
+			GraphicsConfiguration gc = win.getGraphicsConfiguration(); //get monitor's settings
+			return gc.createCompatibleImage(w,h,t);
 		}
-		vc.setFullScreenWindow(null); //collapses full screensxcvsdaswe4ol
+		return null;
 	}
 }
